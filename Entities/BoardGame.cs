c1227a88056e2cc0a5afcbcfffe2f49c3174e789
THEjoezack <me@joezack.com>
2018-03-18 17:20:13 -0400

    Moving stuff around

---
+++ Entities/BoardGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+namespace Entities
+{
+    public class BoardGame
+    {
+      long Id { get; set; }
+      string Game { get; set; }
+      decimal Rating { get; set; }
+      int time { get; set; }
+      int MinPlayers { get; set; }
+      int MaxPlayers { get; set; }
+      int RecommendedPlayers { get; set; }
+      IEnumerable<string> Mechanics { get; set; }
+      string BggLink { get; set; }
+    }
+}
+}
