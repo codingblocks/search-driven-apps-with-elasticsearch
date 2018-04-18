ff4a2b98605e8cdd5b55c962f66d72a68d65c5fe
THEjoezack <me@joezack.com>
2018-04-18 09:08:03 -0400

    Updating flask template, aggregates

--- README.md
+++ README.md
@@ -34,3 +34,94 @@ docker-compose -f docker-compose-dev.yml up```
 
 Why does this seem wrong?
 If I use docker-compose up and python crashes, I have to restart everything?
+
+```Useful queries:
+# Mappings
+DELETE /games
+PUT /games
+{
+ "mappings": {
+  "boardgame": {
+   "properties": {
+      "id": {"type": "integer"},
+      "title": {"type": "text"},
+      "rating": {"type": "float"},
+      "time": {"type": "integer"},
+      "minimumPlayers": {"type": "integer"},
+      "maximumPlayers": {"type": "integer"},
+      "recommendedPlayers": {"type": "integer"},
+      "mechanics": { "type": "text" },
+      "demo": {"type": "boolean"},
+      "wantsToPlay": { "type": "keyword" },
+      "owns": {"type": "boolean"},
+      "played": {"type": "boolean"},
+      "boardGameGeekLink": {"type": "text"}
+   }
+  }
+ }
+}
+# Make sure the search is there..
+GET /games
+# Search via match (don't do this)
+GET /games/_search
+{
+  "query": {
+    "match": { 
+      "wantsToPlay": "Carrie" }
+  }
+}
+# Get just one
+GET /games/_search
+{
+    "query": {
+      "term" : {
+          "wantsToPlay" : "Josh"
+      }
+    }
+}
+# UNION
+GET /games/_search
+{
+    "query": {
+      "terms" : {
+          "wantsToPlay" : ["Josh","Carrie"]
+      }
+    }
+}
+# INTERSECTION
+GET /games/_search
+{
+    "query": {
+    "bool": {
+      "filter": [
+        { "term": { "wantsToPlay" : "Josh" } },
+        { "term": { "wantsToPlay" : "Carrie" } }
+      ]
+    }
+  }
+}
+# Aggregates, the entire matrix
+POST /games/_search?size=0
+{
+  "size": 0,
+  "aggs" : {
+    "interactions" : {
+      "adjacency_matrix" : {
+        "filters" : {
+          "Josh" : { "term": { "wantsToPlay" : "Josh" } },
+          "Carrie" : { "term": { "wantsToPlay" : "Carrie" } },
+          "Joe" : { "term": { "wantsToPlay" : "Joe" } }
+        }
+      }
+    }
+  }
+}
+# Aggregations
+GET /_search
+{
+    "aggs" : {
+        "genres" : {
+            "terms" : { "field" : "wantsToPlay" }
+        }
+    }
+}```
--- flask/app.py
+++ flask/app.py
@@ -5,33 +5,30 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-    data = json.dumps({
-                "query": {
-                "bool": {
-                "filter": [
-                    { "term": { "wantsToPlay" : "Josh" } },
-                    { "term": { "wantsToPlay" : "Carrie" } }
-                ]
-            }
-        }
-    })
-    search_results = requests.get('http://elasticsearch:9200/games/_search', headers={'content-type': 'application/json'}, data=data)
-    results = json.loads(search_results.content) # deserialize the content so we can access things via python
-    return render_template("index.html",hits=results['hits'])
+  search_results = requests.get('http://elasticsearch:9200/games/_search', headers={'content-type': 'application/json'}, data=import_json('data/basic_search.json'))
+  results = json.loads(search_results.content) # deserialize the content so we can access things via python
+  paging = {
+    'showPaging': True,
+    'currentPage': 1,
+    'startPage': 1,
+    'endPage': 10,
+    'totalPages': 100
+  }
+  return render_template("index.j2", hits=results['hits'], aggregations=results['aggregations'], paging=paging)
 
 @app.route('/reset')
 def reset():
-    responses = {
-        'delete': requests.delete('http://elasticsearch:9200/games', headers={'content-type': 'application/json'}),
-        'put': requests.put('http://elasticsearch:9200/games', headers={'content-type': 'application/json'}, data=import_json('data/mapping_config.json')),
-        'post': requests.post('http://elasticsearch:9200/games/doc/_bulk', headers={'content-type': 'application/x-ndjson'}, data=open('data/games-formatted.txt').read())
-    }
-    return render_template("reset.html", responses=responses)
+  responses = {
+    'delete': requests.delete('http://elasticsearch:9200/games', headers={'content-type': 'application/json'}),
+    'put': requests.put('http://elasticsearch:9200/games', headers={'content-type': 'application/json'}, data=import_json('data/mapping_config.json')),
+    'post': requests.post('http://elasticsearch:9200/games/doc/_bulk', headers={'content-type': 'application/x-ndjson'}, data=open('data/games-formatted.txt').read())
+  }
+  return render_template("reset.j2", responses=responses)
 
 def import_json(filename):
-    file_data = open(filename).read()
-    json_data = json.loads(file_data)
-    return json.dumps(json_data, indent=2, sort_keys=True) # Make it pretty!
+  file_data = open(filename).read()
+  json_data = json.loads(file_data)
+  return json.dumps(json_data, indent=2, sort_keys=True) # Make it pretty!
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
