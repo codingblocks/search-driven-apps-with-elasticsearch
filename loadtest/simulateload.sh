82aa97380ecd8bed59a4086224f211fbd4aff702
THEjoezack <me@joezack.com>
2018-09-12 07:16:56 -0400

    Adding load test script, minor tweaks to pptx

---
+++ loadtest/simulateload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while :
+do
+	curl "http://localhost:5000/?search=Dockr" > /dev/null
+	sleep 1
+    curl "http://localhost:5000/?tags=Coding+Blocks" > /dev/null
+    sleep 1
+    curl "http://localhost:5000/?tags=Coding+Blocks&search=fff"  > /dev/null # this throws an exception!
+    sleep 1
+    curl "http://localhost:5000/?search=$RANDOM" > /dev/null
+    sleep 1
+done
